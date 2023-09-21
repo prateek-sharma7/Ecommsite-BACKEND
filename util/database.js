084d5e34a9e98660529dfb502e530402f77102a9
Prateek <sharmaprateek499@gmail.com>
2023-09-21 20:21:41 +0530

    fetching all products and working with compass GUI

--- util/database.js
+++ util/database.js
@@ -5,7 +5,7 @@ let _db;
 
 const mongoConnect = (callback) => {
   MongoClient.connect(
-    "mongodb+srv://nodeuser:hLbN2eeFtSnFNahX@cluster0.djjzen1.mongodb.net/?retryWrites=true&w=majority"
+    "mongodb+srv://nodeuser:hLbN2eeFtSnFNahX@cluster0.djjzen1.mongodb.net/shop?retryWrites=true&w=majority"
   )
     .then((client) => {
       console.log("Connected!");
