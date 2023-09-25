55dc3a78b3f35278730aefffdadb93a8d8838beb
Prateek <sharmaprateek499@gmail.com>
2023-09-25 23:06:35 +0530

    adding user and cart model

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const errorController = require("./controllers/error");
-
 const mongoConnect = require("./util/database").mongoConnect;
 const User = require("./models/user");
 
@@ -20,9 +19,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use((req, res, next) => {
-  User.findById("6510476174cdbce311f6ca5d")
+  User.findById("651198ac74cdbce311f6ca6b")
     .then((user) => {
-      req.user = user;
+      req.user = new User(user.name, user.email, user.cart, user._id);
       next();
     })
     .catch((err) => console.log(err));
